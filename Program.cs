231b99ff85a9e82605adccc15e9ccf0351938a78
AlexH528814 <528814@escg.ac.uk>
2022-09-16 20:14:22 +0100

    added code + image
    Added image of Hello World loop in action with code, and the file of the code itself 20:33 16/09/22

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+
+            while (i > 0)
+            {
+                Console.WriteLine("Hello World");
+            }
+
+        }
+    }
+}
+
