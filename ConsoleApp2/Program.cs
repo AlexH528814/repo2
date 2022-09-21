1a6373a21f2dd91498bff0468176ec3caf3bb3a1
AlexH528814 <528814@escg.ac.uk>
2022-09-21 20:07:41 +0100

    practise code
    solo learn practise

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int x = 4; int y = 9;
+            x = (y % x != 0) ? y / x : y;
+            Console.WriteLine(x);
+        }
+    }
+}
